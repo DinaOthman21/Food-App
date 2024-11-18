a04fb9e9da85852764b4fffcb7773ccec77188fa
dina othman <dinaosman581@gmail.com>
2024-11-18 18:03:46 +0200

    add MealRepository

--- app/src/main/java/com/example/food_app/data/repository/MealRepositoryImpl.kt
+++ app/src/main/java/com/example/food_app/data/repository/MealRepositoryImpl.kt
@@ -1,8 +1,15 @@
 package com.example.food_app.data.repository
 
+import coil.network.HttpException
 import com.example.food_app.data.remote.ApiServices
+import com.example.food_app.data.remote.dto.popularMeal.CategoryMeal
 import com.example.food_app.data.remote.dto.randomMeal.MealList
 import com.example.food_app.domain.MealRepository
+import com.example.food_app.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+
 
 class MealRepositoryImpl (
     private val api : ApiServices
@@ -11,5 +18,32 @@ class MealRepositoryImpl (
     override suspend fun getRandomMeal(): MealList {
         return api.getRandomMeal()
     }
+
+    override suspend fun getPopularItems(categoryName: String): Flow<Resource<List<CategoryMeal>>> {
+        return flow {
+            emit(Resource.Loading(true))
+            val popularItemsList = try {
+                api.getPopularItems(
+                    categoryName= categoryName
+                )
+            } catch (e: IOException){
+                e.printStackTrace()
+                emit(Resource.Error(message = "Loading Error" , throwable = e))
+                return@flow
+            }  catch (e : HttpException){
+                e.printStackTrace()
+                emit(Resource.Error(message = "Loading Error" , throwable = e))
+                return@flow
+            } catch (e : Exception){
+                e.printStackTrace()
+                emit(Resource.Error(message = "Loading Error" , throwable = e))
+                return@flow
+            }
+            val popularItems = popularItemsList.meals
+            emit(Resource.Success(popularItems))
+            emit(Resource.Loading(false))
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/food_app/domain/MealRepository.kt
+++ app/src/main/java/com/example/food_app/domain/MealRepository.kt
@@ -1,7 +1,13 @@
 package com.example.food_app.domain
 
+import com.example.food_app.data.remote.dto.popularMeal.CategoryMeal
 import com.example.food_app.data.remote.dto.randomMeal.MealList
+import com.example.food_app.util.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface MealRepository {
     suspend fun getRandomMeal() : MealList
+    suspend fun getPopularItems(
+        categoryName : String
+    ) : Flow<Resource<List<CategoryMeal>>>
 }
