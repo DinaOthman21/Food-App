47d19ff4c225e33471c06959153a4ec1bae5deae
dina othman <dinaosman581@gmail.com>
2024-12-02 18:28:38 +0200

    add CategoriesList to HomeScreen

--- app/src/main/java/com/example/food_app/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/food_app/presentation/home/HomeScreen.kt
@@ -1,8 +1,8 @@
 package com.example.food_app.presentation.home
 
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,69 +17,76 @@ fun HomeScreen(
     onMealClick:(Meal) -> Unit,
     onPopularItemClick: (MealByCategory)-> Unit
 ) {
-    Column(
+    LazyColumn (
         modifier = Modifier
             .fillMaxSize()
     ) {
-        HomeHeader()
+        item {
+            HomeHeader()
+        }
+
         // random Meal
-        when {
-            state.randomIsLoading -> {
-                Text(text = "Random Meal Is Loading...")
-            }
-            state.randomError != null -> {
-                Text(text = "Error: ${state.randomError}")
-            }
-            state.randomMeal != null -> {
+        item {
+            when {
+                state.randomIsLoading -> {
+                    Text(text = "Random Meal Is Loading...")
+                }
+                state.randomError != null -> {
+                    Text(text = "Error: ${state.randomError}")
+                }
+                state.randomMeal != null -> {
                     HomeRandomMeal(
                         state = state,
                         onMealClick = onMealClick
                     )
-                    PopularItemsScreen(
-                        state = state,
-                        onItemClick = onPopularItemClick
-                    )
-            }
-            else -> {
-                Text(text = "No random meal available.")
+                }
+                else -> {
+                    Text(text = "No random meal available.")
+                }
             }
         }
 
         // popular items
-        when {
-            state.popularIsLoading -> {
-                Text(text = "Popular Items IsLoading...")
-            }
-            state.popularError != null -> {
-                Text(text = "Error: ${state.popularError}")
-            }
-            state.popularItems.isNotEmpty() -> {
-                PopularItemsScreen(
-                    state = state,
-                    onItemClick = onPopularItemClick
-                )
-            }
-            else -> {
-                Text(text = "No popular items available.")
+        item {
+            when {
+                state.popularIsLoading -> {
+                    Text(text = "Popular Items IsLoading...")
+                }
+                state.popularError != null -> {
+                    Text(text = "Error: ${state.popularError}")
+                }
+                state.popularItems.isNotEmpty() -> {
+                    PopularItemsScreen(
+                        state = state,
+                        onItemClick = onPopularItemClick
+                    )
+                }
+                else -> {
+                    Text(text = "No popular items available.")
+                }
             }
         }
 
         //categories
-        when {
-            state.categoryIsLoading -> {
-                Text(text = "Categories Is Loading...")
-            }
-            state.categoryError != null -> {
-                Text(text = "Error: ${state.categoryError}")
-            }
-            state.categoriesList.isNotEmpty() -> {
-                CategoryListScreen(
-                    state = state
-                )
-            }
-            else -> {
-                Text(text = "No categories available.")
+        item {
+            when {
+                state.categoryIsLoading -> {
+                    Text(text = "Categories Is Loading...")
+                }
+                state.categoryError != null -> {
+                    Text(text = "Error: ${state.categoryError}")
+                }
+                state.categoriesList.isNotEmpty() -> {
+                    CategoryListScreen(
+                        state = state
+                    )
+                }
+                else -> {
+                    Text(text = "No categories available.")
+                }
             }
         }
+
+
     }
 }
--- app/src/main/java/com/example/food_app/presentation/home/categories/CategoryListScreen.kt
+++ app/src/main/java/com/example/food_app/presentation/home/categories/CategoryListScreen.kt
@@ -9,10 +9,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.food_app.presentation.common.CategoryIcon
 import com.example.food_app.presentation.home.HomeState
 
@@ -20,21 +25,31 @@ import com.example.food_app.presentation.home.HomeState
 fun CategoryListScreen(
     state : HomeState
 ) {
+    Text(
+        text = "Categories",
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.secondary,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(start = 30.dp, top = 20.dp)
+    )
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.padding(start = 30.dp, end = 30.dp).fillMaxSize()
+        modifier = Modifier
+            .padding(start = 30.dp, end = 30.dp)
+            .height(500.dp)
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(vertical = 8.dp, horizontal = 4.dp),
-            contentPadding = PaddingValues(vertical = 8.dp, horizontal = 4.dp)
-        ) {
-            items(state.categoriesList.size) { index ->
-                CategoryIcon(state.categoriesList[index])
-                Spacer(modifier = Modifier.height(16.dp))
-            }
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 8.dp, horizontal = 4.dp),
+        contentPadding = PaddingValues(vertical = 8.dp, horizontal = 4.dp)
+    ) {
+        items(state.categoriesList.size) { index ->
+            CategoryIcon(state.categoriesList[index])
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
+}
 }
